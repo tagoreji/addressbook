6855450ba0b54b4886e4e35f898b90c4f51e640e
Preethi Dovala <preethidovala@Preethis-MacBook-Pro.local>
2024-04-26 20:17:22 +0530

    ansible code

--- server-script.sh
+++ server-script.sh
@@ -8,11 +8,11 @@ if [ -d "addressbook" ]
 then
   echo "repo is cloned and exists"
   cd /home/ec2-user/addressbook
-  git pull origin april-ansible
+  git pull origin april-anisble
 else
   git clone  https://github.com/preethid/addressbook.git
   cd addressbook
-  git checkout april-ansible
+  git checkout april-anisble
 fi
 
 sudo docker build -t $1:$2 /home/ec2-user/addressbook
